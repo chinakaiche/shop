937df2b55f8fa5d200ca69468241a6597dc5027a
chinakaiche <2232041670@qq.com>
2018-06-21 20:27:33 +0800

    用户界面-商品列表

--- app/Admin/Controllers/ProductsController.php
+++ app/Admin/Controllers/ProductsController.php
@@ -29,7 +29,34 @@ class ProductsController extends Controller
         });
     }
 
+    /**
+     * Edit interface.
+     *
+     * @param $id
+     * @return Content
+     */
+    public function edit($id)
+    {
+        return Admin::content(function (Content $content) use ($id) {
 
+            $content->header('编辑商品');
+            $content->body($this->form()->edit($id));
+        });
+    }
+
+    /**
+     * Create interface.
+     *
+     * @return Content
+     */
+    public function create()
+    {
+        return Admin::content(function (Content $content) {
+
+            $content->header('创建商品');
+            $content->body($this->form());
+        });
+    }
 
     /**
      * Make a grid builder.
@@ -55,5 +82,36 @@ class ProductsController extends Controller
         });
     }
 
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+    protected function form()
+    {
+        return Admin::form(Product::class, function (Form $form) {
 
+            //创建一个文本输入框
+            $form->text('title','商品名称')->rules('required');
+            //创建一个图片选择框
+            $form->image('image','封面图片')->rules('required|image');
+            //创建一个富文本编辑器
+            $form->editor('description','商品描述')->rules('required');
+            //创建一组单选框
+            $form->radio('on_sale','上架')->options(['1'=>'是','0'=>'否'])->default('0');
+
+            //添加一对多的关联模型
+            $form->hasMany('skus','SKU',function (Form\NestedForm $form){
+                $form->text('title','SKU 名称')->rules('required');
+                $form->text('description','SKU 描述')->rules('required');
+                $form->text('price','单价')->rules('required|numeric|min:0.01');
+                $form->text('stock','剩余库存')->rules('required|integer|min:0');
+            });
+            $form->saving(function (Form $form){
+                $form->model()->price = collect($form->input('skus'))->where(Form::REMOVE_FLAG_NAME, 0)->min('price');
+            });
+
+
+        });
+    }
 }
